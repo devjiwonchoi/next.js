3dc61e36856ba4d7379c8c9e35636d20ae1f2fdb
devjiwonchoi <devjiwonchoi@gmail.com>
2025-03-14 12:50:05 +0100

    publish script

--- scripts/check-is-release.js
+++ scripts/check-is-release.js
@@ -2,23 +2,30 @@ const { execSync } = require('child_process')
 
 const checkIsRelease = async () => {
   let commitId = process.argv[2] || ''
+  const commitIdArg = commitId ? ` ${commitId}` : ''
 
   // parse only the last string which should be version if
   // it's a publish commit
-  const commitMsg = execSync(
-    `git log -n 1 --pretty='format:%B'${commitId ? ` ${commitId}` : ''}`
+  const commitMsg = execSync(`git log -n 1 --pretty='format:%B'${commitIdArg}`)
+    .toString()
+    .trim()
+
+  const commitAuthor = execSync(
+    `git log -n 1 --pretty='format:%ae'${commitIdArg}`
   )
     .toString()
     .trim()
 
-  const versionString = commitMsg.split(' ').pop().trim()
-  const publishMsgRegex = /^v\d{1,}\.\d{1,}\.\d{1,}(-\w{1,}\.\d{1,})?$/
+  const publishMsgRegex = /^Version Packages \(Patch\/Minor\)$/
+  const isGithubBot =
+    // TODO: vercel-release-bot
+    commitAuthor === 'github-actions[bot]@users.noreply.github.com'
 
-  if (publishMsgRegex.test(versionString)) {
-    console.log(versionString)
+  if (publishMsgRegex.test(commitMsg) && isGithubBot) {
+    console.log(commitMsg)
     process.exit(0)
   } else {
-    console.log('not publish commit', { commitId, commitMsg, versionString })
+    console.log('not publish commit', { commitId, commitMsg, commitAuthor })
     process.exit(1)
   }
 }
